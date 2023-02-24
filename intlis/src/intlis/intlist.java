e710722f62acbf395038e8009aa8cc0fb50c0ad5
Sam <Sam@192.168.0.212>
2023-02-24 19:36:07 +0100

    Arrays

--- intlis/src/intlis/intlist.java
+++ intlis/src/intlis/intlist.java
@@ -1,5 +1,6 @@
 package intlis;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
@@ -10,35 +11,66 @@ import java.util.stream.IntStream;
 public class intlist {
 	
 	/**
+	 * @representationObject
+	 */
+	private int[] integers;
+	
+	/**
 	 * @inspects | this
 	 * @post | result == toIntArray().length
 	 */
-	public int length() { throw new RuntimeException("Not yet implemented");}
+	public int length() { 
+		return toIntArray().length;
+	}
 	
 	/**
 	 * @pre | 0 <= index && index < length()
 	 * @inspects | this
 	 * @post | result == toIntArray()[index]
 	 */
-	public int intAt(int index) { throw new RuntimeException("Not yet implemented");}
+	public int intAt(int index) { 
+		return toIntArray()[index];
+	}
 	
 	/**
 	 * @inspects | this
 	 * @creates | result
 	 * @post | result != null
 	 */
-	public int[] toIntArray() { throw new RuntimeException("Not yet implemented"); }
+	public int[] toIntArray() { 
+		if (integers == null) {
+			int[] temp = new int[0];
+			return temp;
+		} 
+		else {
+			return integers.clone();
+		}
+	}
+	
 	/**
-	 * @pre | number >= 0 && number < length()
 	 * @mutates |
-	 * @post | toIntArray()[-1] == number 
+	 * @post | toIntArray()[length() - 1] == number 
 	 * @post | IntStream.range(0, length() - 2).allMatch(i -> toIntArray()[i] == old(toIntArray())[i])
 	 */
-	public void addInt(int number) { }
+	public void addInt(int number) { 
+		int lengte = length();
+		int[] temp = new int[lengte + 1];
+		int i = 0;
+		while (i < lengte) {
+			temp[i] = toIntArray()[i];
+			i++;
+		}
+		temp[lengte] = number;
+		integers = temp;
+	}
+	
 	/**
 	 * @mutates |
 	 * @post | old(toIntArray()).length == toIntArray().length + 1
 	 * @post | IntStream.range(0, length() - 1).allMatch(i -> toIntArray()[i] == old(toIntArray())[i])
 	 */
-	public void removeInt() { }
+	public void removeInt() { 
+		int[] temp = Arrays.copyOfRange(integers, 0, length() - 1);
+		integers = temp;
+	}
 }
